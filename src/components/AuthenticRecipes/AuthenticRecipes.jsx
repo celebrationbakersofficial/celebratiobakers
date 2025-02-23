0188e8ed8cd5b8fedcd9238d4c68dacaaf647696
aeroedgetechnologies <aeroedgetechnologies@gmail.com>
2025-02-23 13:05:12 +0530

    new push

--- src/components/AuthenticRecipes/AuthenticRecipes.jsx
+++ src/components/AuthenticRecipes/AuthenticRecipes.jsx
@@ -5,18 +5,18 @@ const AuthenticRecipes = () => {
   return (
     <div className="authentic-recipes">
         <div className="heading left w-full lg:w-1/2 p-4">
-          <h1 className=" text-4xl font-bold text-gray-800">Food of The Gods, Freshly Baked!
+          <h1 className="headings text-4xl font-bold text-gray-800">Food of The Gods, Freshly Baked!
           </h1>
           <p className="text-lg text-gray-600 mt-4">
           Since 2004, we've been serving our guests the best quality treats, traditionally made and presented with care.          </p>
         </div>
 <div className="content">
         <div className="left w-full lg:w-1/2 p-4">
-          <h1 className="text-4xl font-bold text-gray-800">AUTHENTIC RECIPES</h1>
+          <h1 className="headings text-4xl font-bold text-gray-800">AUTHENTIC RECIPES</h1>
           <p className="text-lg text-gray-600 mt-4">
             Our products are based on traditional home-style recipes using fresh ingredients.
           </p>
-          <h2 className="text-2xl font-semibold text-gray-700 mt-6">BAKED WITH LOVE</h2>
+          <h2 className="headings text-2xl font-semibold text-gray-700 mt-6">BAKED WITH LOVE</h2>
           <p className="text-lg text-gray-600 mt-2">
             Our passion for baking is poured into every recipe, serving smiles on a plate every day.
           </p>
@@ -38,11 +38,11 @@ const AuthenticRecipes = () => {
         {/* <a href="#" className="know-more-btn">Know More</a> */}
       </div>
         <div className="right w-full lg:w-1/2 p-4">
-          <h3 className="text-3xl font-semibold text-gray-700 mb-4">Food of The Gods, Freshly Baked!</h3>
+          <h3 className="headings text-3xl font-semibold text-gray-700 mb-4">Food of The Gods, Freshly Baked!</h3>
           <p className="text-lg text-gray-600">
             Since 2004, we've been serving our guests the best quality treats, traditionally made and presented with care.
           </p>
-          <h3 className="text-3xl font-semibold text-gray-700 mb-4">Food of The Gods, Freshly Baked!</h3>
+          <h3 className="headings text-3xl font-semibold text-gray-700 mb-4">Food of The Gods, Freshly Baked!</h3>
           <p className="text-lg text-gray-600">
             Since 2004, we've been serving our guests the best quality treats, traditionally made and presented with care.
           </p>
