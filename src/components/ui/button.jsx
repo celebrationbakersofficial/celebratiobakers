f2c64ad45be1d2e42d56d0b04608afeb729c3aed
aeroedgetechnologies <aeroedgetechnologies@gmail.com>
2025-03-16 17:23:50 +0530

    new design changes for mobile devices

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,13 +1,12 @@
-// Button.jsx
-const Button = ({ children, onClick, className }) => {
-  return (
-    <button 
-      onClick={onClick} 
-      className={`px-4 py-2 bg-brown text-white rounded ${className}`}
-    >
-      {children}
-    </button>
-  );
-};
-
-export default Button;
+function Button({ children, onClick, className }) {
+    return (
+      <button 
+        onClick={onClick} 
+        className={`px-4 py-2 bg-brown text-white rounded ${className}`}
+      >
+        {children}
+      </button>
+    );
+  }
+  
+  export { Button };  
