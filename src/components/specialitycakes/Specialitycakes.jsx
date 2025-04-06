9d6e243fd4b5e869611fa29aaaa40d48bf9b4c8a
aeroedgetechnologies <aeroedgetechnologies@gmail.com>
2025-04-06 13:00:03 +0530

    new design changes for mobile devices

--- src/components/specialitycakes/Specialitycakes.jsx
+++ src/components/specialitycakes/Specialitycakes.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer";
-import Product1 from "./alex-lvrs-aX_ljOOyWJY-unsplash.jpg";
-import Product2 from "./american-heritage-chocolate-ghtwiDUu5ro-unsplash.jpg";
-import Product3 from "./honey-yanibel-minaya-cruz-fPWxYxfBVYM-unsplash.jpg";
-import Product4 from "./jacob-thomas-6jHpcBPw7i8-unsplash.jpg";
+import Product1 from "./images/chocolate-cake-with-coffee-cup.jpg";
+import Product2 from "./images/cup-tea-near-homemade-cheesecake-with-fresh-berries-mint-dessert-table.jpg";
+import Product3 from "./images/top-view-cup-coffee-plate-chocolate-cake-wooden-table.jpg";
+import Product4 from "./images/easter-carrot-cake-with-frosting-blue-table.jpg";
+import sectionimage1 from "./section images/photo_2025-04-06_12-39-48.jpg";
+import sectionimage2 from "./section images/photo_2025-04-06_12-48-46.jpg";
 import OrderSection from "../ordersection/OrderSection";
 import { Link } from "react-router-dom";
 import ScrollToTopButton from "../scrolltotop/ScrollToTopButton";
@@ -18,26 +20,22 @@ import ScrollToTopButton from "../scrolltotop/ScrollToTopButton";
   const cakes = [
     {
       name: "After Nine Cake",
-      image:
-        "//theobroma.in/cdn/shop/files/AfterNine_Cake_2_2_Large_01c569f9-38b4-4a52-b136-eae7e4520f8f.jpg?v=1699625065",
+      image: Product1,
       link: "/products/afternine-cake",
     },
     {
       name: "Almond Nougatine Cake",
-      image:
-        "//theobroma.in/cdn/shop/files/Chocolate_Nougatine_Cake_4_Large_4e6a98d8-94f4-4b59-9543-fa93071ba677.jpg?v=1705733220",
+      image:Product2,
       link: "/products/almond-nougatine-cake",
     },
     {
       name: "Cappuccino Cake",
-      image:
-        "//theobroma.in/cdn/shop/files/Cappuccino_Cake_4_Large_404e96df-9134-43ed-be8b-67932e0c4f1b.jpg?v=1699625445",
+      image:Product3,
       link: "/products/cappuccino-cake",
     },
     {
       name: "Carrot Cake",
-      image:
-        "//theobroma.in/cdn/shop/files/Carrot_Cake_2_1_Large_f27d85bd-b0c9-4d79-852b-277daf80eb0e.jpg?v=1699625481",
+      image:Product4,
       link: "/products/carrot-cake",
     },
   ];
@@ -81,9 +79,9 @@ export default function Specialitycakes() {
       className="text-center bg-cover bg-no-repeat bg-bottom py-8 h-[420px]"
       style={{
         backgroundImage:
-          "url(https://cdn.shopify.com/s/files/1/0521/3929/4884/files/specialty-cakes-banner.jpg)",
+          `url(${sectionimage1})`,
         backgroundColor: "#fde4e1",
-        backgroundSize: "contain",
+        backgroundSize: "cover",
       }}
     >
       <div className="max-w-4xl mx-auto">
@@ -127,22 +125,22 @@ export default function Specialitycakes() {
     </div>
       
       {/* Custom Cakes Section */}
-      <div className="bg-[#FEECEA] py-12">
+      <div className="bg-[#f1d3ab] py-12">
       <div className="max-w-6xl mx-auto px-4">
         <div className="text-center mb-8">
-          <h2 className="text-3xl font-bold">Customised Theme Cakes</h2>
+          <h2 className="text-3xl font-bold text-gray-600">Customised Theme Cakes</h2>
         </div>
         <div className="flex flex-col md:flex-row items-center">
           <div className="md:w-2/3 text-left md:order-2">
-            <p className="text-lg">
+            <p className="text-lg text-gray-600">
               We handcraft and customize celebration cakes for all special occasions. From theme-based and personalized birthday cakes to exquisite multi-tiered wedding cakes, we have all your celebrations covered!
               <br /><br />
-              Write to us with your customized theme cake requirements at - <a href="mailto:celebrationbakersofficial@gmail.com" className="text-blue-600">celebrationbakersofficial@gmail.com</a>
+              Write to us with your customized theme cake requirements at - <a href="mailto:celebrationbakersofficial@gmail.com" className="text-pink-500">celebrationbakersofficial@gmail.com</a>
             </p>
           </div>
           <div className="md:w-1/3 md:order-1 text-center">
             <img
-              src="//theobroma.in/cdn/shop/files/custom-illustration.png?v=1700213364"
+              src={sectionimage2}
               alt="Custom Theme Cakes"
               className="w-3/4 mx-auto"
             />
