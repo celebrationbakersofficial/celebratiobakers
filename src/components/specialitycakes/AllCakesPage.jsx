dfa88c264e0aa0a55b2f61bb6a4861803bbe959e
celebrationbakersofficiale <celebrationbakersofficial@gmail.com>
2025-04-12 10:14:31 +0530

    new design changes for mobile devices

--- src/components/specialitycakes/AllCakesPage.jsx
+++ src/components/specialitycakes/AllCakesPage.jsx
@@ -3,33 +3,33 @@ import { Link } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer';
 import ScrollToTopButton from '../scrolltotop/ScrollToTopButton';
+import Product1 from "./images/chocolate-cake-with-coffee-cup.jpg";
+import Product2 from "./images/cup-tea-near-homemade-cheesecake-with-fresh-berries-mint-dessert-table.jpg";
+import Product3 from "./images/top-view-cup-coffee-plate-chocolate-cake-wooden-table.jpg";
+import Product4 from "./images/easter-carrot-cake-with-frosting-blue-table.jpg";
 
-const cakes = [
-  {
-    name: "After Nine Cake",
-    image:
-      "//theobroma.in/cdn/shop/files/AfterNine_Cake_2_2_Large_01c569f9-38b4-4a52-b136-eae7e4520f8f.jpg?v=1699625065",
-    link: "/products/afternine-cake",
-  },
-  {
-    name: "Almond Nougatine Cake",
-    image:
-      "//theobroma.in/cdn/shop/files/Chocolate_Nougatine_Cake_4_Large_4e6a98d8-94f4-4b59-9543-fa93071ba677.jpg?v=1705733220",
-    link: "/products/almond-nougatine-cake",
-  },
-  {
-    name: "Cappuccino Cake",
-    image:
-      "//theobroma.in/cdn/shop/files/Cappuccino_Cake_4_Large_404e96df-9134-43ed-be8b-67932e0c4f1b.jpg?v=1699625445",
-    link: "/products/cappuccino-cake",
-  },
-  {
-    name: "Carrot Cake",
-    image:
-      "//theobroma.in/cdn/shop/files/Carrot_Cake_2_1_Large_f27d85bd-b0c9-4d79-852b-277daf80eb0e.jpg?v=1699625481",
-    link: "/products/carrot-cake",
-  },
-];
+  const cakes = [
+    {
+      name: "After Nine Cake",
+      image: Product1,
+      link: "/products/afternine-cake",
+    },
+    {
+      name: "Almond Nougatine Cake",
+      image:Product2,
+      link: "/products/almond-nougatine-cake",
+    },
+    {
+      name: "Cappuccino Cake",
+      image:Product3,
+      link: "/products/cappuccino-cake",
+    },
+    {
+      name: "Carrot Cake",
+      image:Product4,
+      link: "/products/carrot-cake",
+    },
+  ];
 
 function AllCakesPage() {
   return (
